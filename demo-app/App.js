e01033e7f4ba2f4690a114f980a14fe06d1d9b5d
Steve South <stephen.south@caplin.com>
2021-12-19 15:57:40 +0000

    Retriving signon params correctly

--- demo-app/App.js
+++ demo-app/App.js
@@ -1,11 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-url-polyfill/auto";
+
+import { StyleSheet, Text, View } from "react-native";
+import { useState, useEffect } from "react";
+
+const HOST = "http://192.168.1.34:3000";
+const GOOGLE = "/api/auth/signin/google";
+const CSRF = "/api/auth/csrf";
 
 export default function App() {
+  const [log, setLog] = useState(null);
+  const [params, setParams] = useState(null);
+
+  const logger = {
+    log: (arg1 = "", arg2 = "") => {
+      console.log(`${arg1} ${arg2}`);
+      setLog(`${arg1} : ${arg2}`);
+    },
+  };
+
+  useEffect(async () => {
+    try {
+      logger.log(`\nStep 1: Fetching csrf from ${HOST + CSRF}`);
+
+      const csrf = await fetch(HOST + CSRF)
+        .then((response) => response.json())
+        .catch((error) => logger.log("ERROR 1: ", error));
+
+      logger.log("\nStep 2: Retrieved csrf", csrf.csrfToken);
+
+      logger.log(
+        "\nStep 3: Posting to signon to begin the signon process in order retreive code_challenge, redirect_uri and state. Including redirect:false in the body so that we get a response rather than next auth redirecting."
+      );
+
+      logger.log(`Endpoint: ${HOST + GOOGLE}`);
+
+      fetch(HOST + GOOGLE, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          csrfToken: csrf.csrfToken,
+          redirect: false,
+        }),
+      })
+        .then((response) => {
+          logger.log("\nStep 4: signin reponse received");
+          response
+            .json()
+            .then((val) => {
+              logger.log("\nStep 5: signin reponse json parsed correctly");
+
+              const params = new URLSearchParams(val.redirect);
+              const codeChallenge = params.get("code_challenge");
+              const redirectUri = params.get("redirect_uri");
+              const state = params.get("state");
+
+              logger.log("codeChallenge", codeChallenge);
+              logger.log("redirectUri", redirectUri);
+              logger.log("state", state);
+
+              const paramState = {
+                challengePair: {
+                  challenge: codeChallenge,
+                  verifier: null,
+                },
+                redirectUri,
+                state,
+              };
+
+              logger.log(
+                "\nStep 6: setting react state\n",
+                JSON.stringify(paramState, null, 4)
+              );
+
+              setParams(paramState);
+            })
+            .catch((error) => logger.log("ERROR 2:", error));
+        })
+        .catch((error) => console.log("ERROR 3", error));
+    } catch (error) {
+      logger.log("ERROR 4", error);
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>
+        Demonstration of an oauth flow using modified NextAuth and react native
+        expo
+      </Text>
+      <Text>{log}</Text>
     </View>
   );
 }
@@ -13,8 +100,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    paddingTop: 100,
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 14,
+    paddingHorizontal: 40,
   },
 });
